6f4ed57c002f1558df7c965842935dff65bc30ad
Patrick Cullinane <cullinap@gmail.com>
2024-08-08 07:35:25 -0400

    rough draft of random deletions

--- data_generator/src/data_generators/base_generator.py
+++ data_generator/src/data_generators/base_generator.py
@@ -1,4 +1,6 @@
-
+import random
 class BaseGenerator:
     def generate(self, count=1):
         raise NotImplementedError("This method should be overridden by subclasses")
+
+    
--- data_generator/src/data_generators/company_generator.py
+++ data_generator/src/data_generators/company_generator.py
@@ -1,4 +1,5 @@
 from faker import Faker 
+import random
 from .base_generator import BaseGenerator
 
 class CompanyGenerator(BaseGenerator):
@@ -7,5 +8,20 @@ class CompanyGenerator(BaseGenerator):
 
     def generate(self, count=1):
         return [self.fake.company() for _ in range(count)]
+
+    def random_deletion(self, s):
+        if len(s) == 0:
+            return s 
+        index_to_remove = random.randint(0, len(s) - 1)
+        return s[:index_to_remove] + s[index_to_remove + 1:]
+
+    def generate_mix_dataset(self):
+        names_to_be_modified = self.generate(count=2)
+        modified_names = [self.random_deletion(i) for i in names_to_be_modified]
+        static_names = self.generate(count=10)
+
+        return names_to_be_modified + modified_names + static_names
+
+
 
     
